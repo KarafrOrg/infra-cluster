2e87a2c34c1d1d567b7e0d2f4fe1a252eaa534c3
karafra <55988328+karafra@users.noreply.github.com>
2026-06-15 20:03:20 +0200

    Add ELS operator

--- modules/infra-cluster/variables.tf
+++ modules/infra-cluster/variables.tf
@@ -116,14 +116,3 @@ variable "eck_operator" {
   })
   default = {}
 }
-
-variable "eck_operator" {
-  description = "Elastic Cloud on Kubernetes (ECK) operator Helm release configuration."
-  type = object({
-    enabled            = optional(bool, true)
-    release_name       = optional(string, "eck-operator")
-    release_namespace  = optional(string, "infra-elastic")
-    release_repository = optional(string, "https://helm.elastic.co")
-  })
-  default = {}
-}
